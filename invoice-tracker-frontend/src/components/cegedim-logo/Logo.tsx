cf0fca9c9de5febf52d1b665f6128fff411427e9
Abdelrhman Samir <abdulrhman.samir123@gmail.com>
2022-08-16 22:44:37 +0200

    Finishing hr-navbar v1

---
+++ invoice-tracker-frontend/src/components/cegedim-logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/Cegedim_Logo.jpg'
+
+const Logo = () => {
+  return (
+    <a href="#" className="flex items-center">
+        <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-logo-text-color">Cegedim</span>
+    </a>
+
+  )
+}
+
+export default Logo;
--- invoice-tracker-frontend/src/components/navbar/admin-nav-bar.tsx
+++ invoice-tracker-frontend/src/components/navbar/admin-nav-bar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { Menu, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
 import { Z_FIXED } from "zlib";
-import logo from '../../assets/Cegedim_Logo.jpg'
+import BellButton from "../Buttons/BellNotification";
+import Logo from "../cegedim-logo/Logo";
 
 const styles = {
     inset: '0px auto auto 0px',
@@ -10,8 +12,7 @@ const styles = {
 
 /*
   TODOs:
-  - Changing the SVG icons to be filled when clicked, how do I revert them when they are not active?
-  - Adding a drop down item, is it always static and hidden?
+  - Handling the onClickOutside for the notification icon
   - Make a smaller components later (dropdown list - list item)
 */
 
@@ -28,53 +29,48 @@ const Navbar = () => {
       {/* Container of all the items in the navbar */}
       <div className="container flex flex-wrap justify-between md:w-auto mx-auto">
         {/* The logo and company title */}
-        <a href="#" className="flex items-center">
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-logo-text-color">Cegedim</span>
-        </a>
-      
+        <Logo/>
+        
         {/* Container to start from the left {Cegedim Members - Bell Icon - User Icon) */}
         <div className="flex items-center md:order-2">
             <div className = "flex item-center space-x-2" >
               {/* Cegedim Members */}
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-11111111111111gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Cegedim Members</a>
+              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Cegedim Members</a>
+             
               {/* Bill Icon */}
-              <button className="relative border-2 focus:ring-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
+              <BellButton count = {1}/> 
 
-                <span className="absolute inset-0 object-right-top -mr-6">
-                  <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-                      6
-                  </div>
-                </span>
-              </button>      
-              
               {/* Container for the User Icon and the dropdown menu */}
-              <div className="flex items-center relative">
-                <div className="dropdown relative">
-                  {/* User Icon */}
-                  <a className="dropdown-toggle flex items-center hidden-arrow border-2 focus:ring-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" href="#" id="dropdownMenuButton2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 rounded-full" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-                    </svg>
-                  </a>
-
-                  {/* The hidden dropdown menu */}
-                  <ul className="dropdown-menu min-w-max absolute bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0" aria-labelledby="dropdownMenuButton2">
-                    <li>
-                      <a className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100" href="#">Action</a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100" href="#">Another action</a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100" href="#">Something else here</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <Menu as="div" className="ml-3 relative">
+                  <div>
+                    <Menu.Button className="flex text-sm rounded-full focus:outline-none">
+                      <a className="dropdown-toggle flex items-center hidden-arrow border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" href="#" id="dropdownMenuButton2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 rounded-full" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                    </Menu.Button>
+                    </div>
+                    <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                          <a href="#" className='block px-4 py-2 text-sm text-gray-700'>
+                            Your Profile
+                          </a>
+                      </Menu.Item>
+                      <Menu.Item>
+                          <a href="#" className='block px-4 py-2 text-sm text-gray-700'>
+                            Settings
+                          </a>
+                      </Menu.Item>
+                      <Menu.Item>
+                          <a href="#" className='block px-4 py-2 text-sm text-gray-700'>
+                            Sign out
+                          </a>
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
             </div>
         </div>
       </div>
