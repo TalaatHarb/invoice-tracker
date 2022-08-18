44670ec20e4f654ba2f4d35bdb4dfc46373a0923
mzakariia <mohamed.zakariia1@gmail.com>
2022-08-18 03:44:32 +0200

    added types content and empty selector file

--- invoice-tracker-frontend/src/components/employees-hub/types.ts
+++ invoice-tracker-frontend/src/components/employees-hub/types.ts
@@ -1,1 +1,57 @@
-import React from "react";
+export type columnTypes = {
+  id: boolean;
+  nationalId: boolean;
+  englishName: boolean;
+  arabicName: boolean;
+  englishAddress: boolean;
+  arabicAddress: boolean;
+  jobTitle: boolean;
+  joiningDate: boolean;
+  endDate: boolean;
+  allowedBalance: boolean;
+  remainingBalance: boolean;
+  billable: boolean;
+  isDisabled: boolean;
+  team: boolean;
+  fulltime: boolean;
+};
+
+export type employeeType = {
+  id: number;
+  nationalId: string;
+  englishName: string;
+  arabicName: string;
+  englishAddress: string;
+  arabicAddress: string;
+  jobTitle: string;
+  joiningDate: Date;
+  endDate: Date;
+  allowedBalance: number;
+  remainingBalance: number;
+  billable: boolean;
+  isDisabled: boolean;
+  team: string[];
+  fulltime: boolean;
+};
+
+export type employeesType = {
+  employees: {
+    id: number;
+    nationalId: string;
+    englishName: string;
+    arabicName: string;
+    englishAddress: string;
+    arabicAddress: string;
+    jobTitle: string;
+    joiningDate: Date;
+    endDate: Date;
+    allowedBalance: number;
+    remainingBalance: number;
+    billable: boolean;
+    isDisabled: boolean;
+    team: string[];
+    fulltime: boolean;
+  }[];
+};
+
+export default columnTypes;
