ba2d81c1936678cb23cb63a75617fe0deb5890eb
amr_essam111 <amr@energenious.eu>
2022-08-23 18:49:19 +0200

    fixing error

--- invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/dtos/managerRequest.java
+++ invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/dtos/managerRequest.java
@@ -1,6 +1,17 @@
 package net.talaatharb.invoicetracker.dtos;
 
+import lombok.Data;
 import net.talaatharb.invoicetracker.models.Request;
 
-public record managerRequest(String requestedBy, Request request) {
+@Data
+public class managerRequest {
+    
+    private String requestedBy;
+   
+     private Request request;
+
+    public managerRequest(String requestedBy, Request request) {
+        this.requestedBy=requestedBy;
+        this.request=request;
+    }
 }
