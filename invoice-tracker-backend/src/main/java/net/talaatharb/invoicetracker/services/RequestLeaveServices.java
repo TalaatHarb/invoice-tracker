31b99e8635145cdffb435da6863e8c2c7428db11
khaledharb1313 <khaledharb1313 @gmail.com>
2022-08-24 14:55:37 +0200

    edited services and merged controllers

--- invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/services/RequestLeaveServices.java
+++ invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/services/RequestLeaveServices.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.talaatharb.invoicetracker.dtos.RequestLeaveBody;
@@ -50,6 +51,7 @@ public class RequestLeaveServices {
 
 		return requestsBody;
 	}
+    @Transactional
 
 	public Request update_a_leave_request(int requestId, Request request) {
 		Request requestFromDB = requestRepo.findById(requestId).get();
