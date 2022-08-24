55f6b82114a8efb046b42c66c872d39601dd0856
Ahmed Elsayed <ahm.abdelwahab@nu.edu.eg>
2022-08-24 02:06:38 +0200

    frontend style absence request

--- invoice-tracker-frontend/src/components/Absence Request/index.tsx
+++ invoice-tracker-frontend/src/components/Absence Request/index.tsx
@@ -69,57 +69,87 @@ const RequestForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Absenece Type:
-        <select value={request.type} name="type" onChange={handleChange}>
-          <option value="annual leave">Annual leave</option>
-          <option value="sick leave">Sick leave</option>
-          <option value="bereavement leave">Bereavement leave</option>
-        </select>
-      </label>
-      <label>
-        Start Date:
+    <div className="containerr py-10 h-screen b bg-lightGrey">
+      <form
+        className="mx-auto pb-20 px-10 rounded-2xl bg-white w-4/5"
+        onSubmit={handleSubmit}
+      >
+        <div className="font-semibold w-full h-16 mb-10 p-5 text-white rounded-xl text-xl bg-blueCegedim text-center">
+          Absence Request
+        </div>
+        <label className="block mb-2 mx-auto md:w-6/12 text-lg font-medium text-black dark:text-lightGrey">
+          Absenece Type:
+          <select
+            value={request.type}
+            name="type"
+            onChange={handleChange}
+            className="inline-block p-2 mb-6 ml-2 md:w-50 text-md text-darkGrey bg-white rounded-lg border border-darkGrey focus:blueCegedim focus:darkBlue dark:darkGrey dark:placeholder-yellowDarkCegedim dark:text-white dark:focus:darkBlue dark:focus:blueCegedim"
+          >
+            <option value="annual leave">Annual leave</option>
+            <option value="sick leave">Sick leave</option>
+            <option value="bereavement leave">Bereavement leave</option>
+          </select>
+        </label>
+
+        <label className="float-left mb-2 mx-auto text-lg font-medium text-black dark:text-lightGrey">
+          Start Date:
+          <input
+            type="date"
+            name="startDate"
+            min={new Date().toISOString().slice(0, 10)}
+            required
+            onChange={(e) =>
+              setRequest({ ...request, startDate: e.target.value })
+            }
+            className="inline-block p-2 mb-6 ml-2 text-sm text-darkGrey bg-white rounded-lg border border-darkGrey focus:blueCegedim focus:darkBlue dark:darkGrey dark:placeholder-yellowDarkCegedim dark:text-white dark:focus:darkBlue dark:focus:blueCegedim"
+          />
+        </label>
+
+        <label>
+          <select
+            value={request.fullDay ? "full day" : "half day"}
+            name="fullDay"
+            onChange={handleChange}
+            className="md:float-right md:w-2/5 p-2 mb-6 md:ml-4 text-lg text-darkGrey bg-white rounded-lg border border-darkGrey focus:blueCegedim focus:darkBlue dark:darkGrey dark:placeholder-yellowDarkCegedim dark:text-white dark:focus:darkBlue dark:focus:blueCegedim"
+          >
+            <option value="full day">Full day</option>
+            <option value="half day">Half day</option>
+          </select>
+        </label>
+        <div className="clear-both"></div>
+        <label className=" mb-2 text-lg font-medium text-black dark:text-lightGrey">
+          End Date:
+          <input
+            type="date"
+            name="endDate"
+            min={request.startDate}
+            required
+            onChange={(e) =>
+              setRequest({ ...request, endDate: e.target.value })
+            }
+            className="inline-block p-2 mb-6 ml-2 text-sm text-darkGrey bg-white rounded-lg border border-darkGrey focus:blueCegedim focus:darkBlue dark:darkGrey dark:placeholder-yellowDarkCegedim dark:text-white dark:focus:darkBlue dark:focus:blueCegedim"
+          />
+        </label>
+
+        <label className="block mb-2 text-lg font-medium text-black dark:text-lightGrey">
+          Notes:
+          <textarea
+            className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={request.comment}
+            name="comment"
+            onChange={(e) =>
+              setRequest({ ...request, comment: e.target.value })
+            }
+          />
+        </label>
+
         <input
-          type="date"
-          name="startDate"
-          min={new Date().toISOString().slice(0, 10)}
-          required
-          onChange={(e) =>
-            setRequest({ ...request, startDate: e.target.value })
-          }
+          className="mt-5 cursor-pointer float-right text-white bg-blueCegedim hover:bg-darkGrey focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          type="submit"
+          value="Submit"
         />
-      </label>
-      <label>
-        End Date:
-        <input
-          type="date"
-          name="endDate"
-          min={request.startDate}
-          required
-          onChange={(e) => setRequest({ ...request, endDate: e.target.value })}
-        />
-      </label>
-      <label>
-        <select
-          value={request.fullDay ? "full day" : "half day"}
-          name="fullDay"
-          onChange={handleChange}
-        >
-          <option value="full day">Full day</option>
-          <option value="half day">Half day</option>
-        </select>
-      </label>
-      <label>
-        Notes:
-        <textarea
-          value={request.comment}
-          name="comment"
-          onChange={(e) => setRequest({ ...request, comment: e.target.value })}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+      </form>
+    </div>
   );
 };
 
