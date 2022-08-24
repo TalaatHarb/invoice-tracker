375e8eba3fda5bba6c321d8630695f18bf3c520f
Abdelrhman Samir <abdulrhman.samir123@gmail.com>
2022-08-24 11:51:17 +0200

    Connecting to the backend

--- invoice-tracker-frontend/src/App.tsx
+++ invoice-tracker-frontend/src/App.tsx
@@ -1,63 +1,60 @@
-import React from 'react'
-import Login from './pages/Login'
-import AdminPage from './pages/AdminPage'
-import UserPage from './pages/UserPage'
-import HrPage from './pages/HrPage'
-import EmployeePage from './pages/EmployeePage'
-import PrivateRoute from './components/PrivateRoute'
-import { useAppSelector } from './hooks/toolkit-types'
-import Navbar from './components/Navbar'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import ForgotPassword from './pages/password/ForgotPassword'
-import ResetPassword from './pages/password/ResetPassword'
-import AbsenceHistory from './pages/AbsenceHistory'
+import React from "react";
+import Login from "./pages/Login";
+import AdminPage from "./pages/AdminPage";
+import UserPage from "./pages/UserPage";
+import HrPage from "./pages/HrPage";
+import EmployeePage from "./pages/EmployeePage";
+import PrivateRoute from "./components/PrivateRoute";
+import { useAppSelector } from "./hooks/toolkit-types";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ForgotPassword from "./pages/password/ForgotPassword";
+import ResetPassword from "./pages/password/ResetPassword";
+import AbsenceHistory from "./pages/AbsenceHistory";
 
 function App() {
   const { isAuthenticated } = useAppSelector(
     (state) => state.AuthenticationSlice
-  )
+  );
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/absencehistory' element={<AbsenceHistory />}/>
-        
-        {!isAuthenticated && <Route path='/login' element={<Login />} />}
+        <Route path="/absencehistory" element={<AbsenceHistory />} />
+
+        {!isAuthenticated && <Route path="/login" element={<Login />} />}
         {/* protected user page */}
-        <Route path='/user' element={<PrivateRoute />}>
-          <Route path='/user' element={<UserPage />} />
-          </Route>
+        <Route path="/user" element={<PrivateRoute />}>
+          <Route path="/user" element={<UserPage />} />
+        </Route>
 
-         {/* reset password routes */}
-         <Route path="/forgot-password" element={<ForgotPassword/>} />
-        <Route path="/reset-password/:resetToken" element={<ResetPassword />}/>
+        {/* reset password routes */}
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
 
-        {!isAuthenticated && <Route path='/login' element={<Login />} />}
-        <Route path='page1' element={<Navbar />} />
+        {!isAuthenticated && <Route path="/login" element={<Login />} />}
+        <Route path="page1" element={<Navbar />} />
 
         {/* protected user page */}
 
-        <Route path='/hr' element={<PrivateRoute />}>
-          <Route path='/hr' element={<HrPage />} />
-
+        <Route path="/hr" element={<PrivateRoute />}>
+          <Route path="/hr" element={<HrPage />} />
         </Route>
         {/* protected admin page */}
-        <Route path='/admin' element={<PrivateRoute />}>
-          <Route path='/admin' element={<AdminPage />} />
+        <Route path="/admin" element={<PrivateRoute />}>
+          <Route path="/admin" element={<AdminPage />} />
         </Route>
         {/* protected employee page */}
-        <Route path='/employee' element={<PrivateRoute />}>
-          <Route path='/employee' element={<EmployeePage />} />
+        <Route path="/employee" element={<PrivateRoute />}>
+          <Route path="/employee" element={<EmployeePage />} />
         </Route>
         {isAuthenticated ? (
-
-          <Route path='*' element={<Navigate to='/employee' replace />} />
-
+          <Route path="*" element={<Navigate to="/employee" replace />} />
         ) : (
-          <Route path='*' element={<Navigate to='/login' replace />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         )}
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- invoice-tracker-frontend/src/components/Navbar/index.tsx
+++ invoice-tracker-frontend/src/components/Navbar/index.tsx
@@ -1,9 +1,11 @@
-import { Menu, Transition } from '@headlessui/react'
-import React, { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useAppSelector } from '../../hooks/toolkit-types'
-import BellButton from '../Button/BellNotification'
-import Logo from '../company-logo'
+import { Menu, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/toolkit-types";
+import { logoutUser } from "../../services/redux/slices/AuthenticationSlice";
+import Button from "../Button";
+import BellButton from "../Button/BellNotification";
+import Logo from "../company-logo";
 
 /*
   TODOs:
@@ -17,23 +19,23 @@ import Logo from '../company-logo'
 */
 
 const Navbar = () => {
-  const { userRole } = useAppSelector(
-    (state) => state.AuthenticationSlice
-  );
+  const { userRole } = useAppSelector((state) => state.AuthenticationSlice);
+
+  const dispatch = useAppDispatch();
 
   return (
-    <nav className='bg-white border-x-4 border-b-4 border-navbar-border px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
+    <nav className="bg-white border-x-4 border-b-4 border-navbar-border px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       {/* Container of all the items in the navbar */}
-      <div className='container flex flex-wrap justify-between md:w-auto mx-auto'>
+      <div className="container flex flex-wrap justify-between md:w-auto mx-auto">
         {/* The logo and company title */}
         <Logo />
         {/* Container to start from the left {Cegedim Members - Bell Icon - User Icon) */}
-        <div className='flex items-center md:order-2'>
-          <div className='flex item-center space-x-2'>
+        <div className="flex items-center md:order-2">
+          <div className="flex item-center space-x-2">
             {/* Cegedim Members */}
             <Link
-              to='www.google.com'
-              className='block py-2 pr-4 pl-3 active:text-blueCegedim text-darkGrey rounded hover:bg-lightGrey md:hover:bg-transparent md:p-0'
+              to="www.google.com"
+              className="block py-2 pr-4 pl-3 active:text-blueCegedim text-darkGrey rounded hover:bg-lightGrey md:hover:bg-transparent md:p-0"
             >
               Cegedim Members
             </Link>
@@ -42,45 +44,48 @@ const Navbar = () => {
             <BellButton count={1} />
 
             {/* Container for the User Icon and the dropdown menu */}
-            <Menu as='div' className='ml-3 relative'>
+            <Menu as="div" className="ml-3 relative">
               <div>
-                <Menu.Button className='flex text-sm rounded-full focus:outline-none'>
+                <Menu.Button className="flex text-sm rounded-full focus:outline-none">
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-6 h-6'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
                   >
                     <path
-                      fillRule='evenodd'
-                      d='M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z'
-                      clipRule='evenodd'
+                      fillRule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clipRule="evenodd"
                     />
                   </svg>
                 </Menu.Button>
               </div>
               <Transition
                 as={Fragment}
-                enter='transition ease-out duration-100'
-                enterFrom='transform opacity-0 scale-95'
-                enterTo='transform opacity-100 scale-100'
-                leave='transition ease-in duration-75'
-                leaveFrom='transform opacity-100 scale-100'
-                leaveTo='transform opacity-0 scale-95'
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <Menu.Item>
                     <Link
-                      to='/employee'
-                      className='block px-4 py-2 text-sm text-gray-700'
+                      to="/employee"
+                      className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Your Profile
                     </Link>
                   </Menu.Item>
                   <Menu.Item>
                     <Link
-                      to='#'
-                      className='block px-4 py-2 text-sm text-gray-700'
+                      to="/login"
+                      onClick={() => {
+                        dispatch(logoutUser());
+                      }}
+                      className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Sign out
                     </Link>
@@ -92,7 +97,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
