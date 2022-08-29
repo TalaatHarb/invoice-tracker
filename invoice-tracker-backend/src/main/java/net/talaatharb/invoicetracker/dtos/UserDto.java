0c545dcbd9834543fbfcef6c7a660275575c77d7
mzakariia <mohamed.zakariia1@gmail.com>
2022-08-29 16:09:03 +0200

    pulled latest

--- invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/dtos/UserDto.java
+++ invoice-tracker-backend/src/main/java/net/talaatharb/invoicetracker/dtos/UserDto.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
