dbb2177a93fdc3d2ce883db51829fc0932881a57
Mohamed Gado <boogado@yahoo.com>
2022-08-31 14:59:37 +0200

    adding export functionality

--- invoice-tracker-frontend/src/components/invoice-page/InvoiceTable.tsx
+++ invoice-tracker-frontend/src/components/invoice-page/InvoiceTable.tsx
@@ -1,138 +1,144 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from 'react'
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
-} from "@tanstack/react-table";
-import { CheckIcon, XIcon } from "@heroicons/react/solid";
-import ColumnSelect from "./ColumnSelector";
-import { Link } from "react-router-dom";
-import formatDate from "../../utils/FormatDate";
+} from '@tanstack/react-table'
+import { CheckIcon, XIcon } from '@heroicons/react/solid'
+import ColumnSelect from './ColumnSelector'
+import { Link } from 'react-router-dom'
+import formatDate from '../../utils/FormatDate'
+import { useReactToPrint } from 'react-to-print'
 
-const EmployeeTab = ({ employees }: any) => {
-  const data = employees;
-  const [rowSelection, setRowSelection] = React.useState({});
-  const [columnVisibility, setColumnVisibility] = React.useState({});
+const EmployeeTab = ({ employees, ref }: any) => {
+  const data = employees
+  const [rowSelection, setRowSelection] = React.useState({})
+  const [columnVisibility, setColumnVisibility] = React.useState({})
 
-  const columnHelper = createColumnHelper<any>();
+  const columnHelper = createColumnHelper<any>()
 
   const columns = [
     columnHelper.group({
       header: ({ table }) => {
         return (
           <input
-            className="w-4"
-            type="checkbox"
+            className='w-4'
+            type='checkbox'
             {...{
               checked: table.getIsAllRowsSelected(),
               onChange: table.getToggleAllRowsSelectedHandler(),
             }}
           />
-        );
+        )
       },
-      id: "Selection",
+      id: 'Selection',
       cell: ({ row }) => {
         return (
           <input
-            className="w-4"
-            type="checkbox"
+            className='w-4'
+            type='checkbox'
             {...{
               checked: row.getIsSelected(),
               onChange: row.getToggleSelectedHandler(),
             }}
           />
-        );
+        )
       },
     }),
     columnHelper.accessor((row) => row.employeeId, {
-      id: "Employee Id",
-      header: "Employee Id",
+      id: 'Employee Id',
+      header: 'Id',
       cell: (info) => info.getValue(),
     }),
 
     columnHelper.accessor((row) => row.englishName, {
-      id: "English name",
-      header: "English name",
+      id: 'English name',
+      header: 'English name',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.arabicName, {
-      id: "Arabic name",
-      header: "Arabic name",
+      id: 'Arabic name',
+      header: 'Arabic name',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.nationalId, {
-      id: "National Id",
-      header: "National Id",
+      id: 'National Id',
+      header: 'National Id',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.profileNumber, {
-      id: "Profile",
-      header: "Profile No.",
+      id: 'Profile',
+      header: 'Profile No.',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.joiningDate, {
-      id: "Joining date",
-      header: "Joining date",
-      cell: (info) => (info.getValue() ? formatDate(info.getValue()) : "N/A"),
+      id: 'Joining date',
+      header: 'Joining date',
+      cell: (info) => (info.getValue() ? formatDate(info.getValue()) : 'N/A'),
     }),
     columnHelper.accessor((row) => row.numberWD, {
-      id: "number WD",
-      header: "No. of Working Days",
+      id: 'number WD',
+      header: 'No. of Working Days',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.amountWD, {
-      id: "amount WD",
-      header: "Amount/Working Days",
+      id: 'amount WD',
+      header: 'Amount/Working Days',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.numberPH, {
-      id: "number PH",
-      header: "No. of Public Holidays",
+      id: 'number PH',
+      header: 'No. of Public Holidays',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.amountPH, {
-      id: "amount PH",
-      header: "Amount/Public Holiday",
+      id: 'amount PH',
+      header: 'Amount/Public Holiday',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.team, {
-      id: "Teams",
-      header: "Team Name",
+      id: 'Teams',
+      header: 'Team Name',
       cell: (info) =>
         info.getValue() && info.getValue().length ? (
           info.getValue().map((team: any) => {
             return (
               <Link
-                className="text-black no-underline block hover:underline"
+                className='text-black no-underline block hover:underline'
                 key={team.id + team.name}
-                to={"/team/" + team.id}
+                to={'/team/' + team.id}
               >
                 {team.name}
               </Link>
-            );
+            )
           })
         ) : (
           <Link
-            className="text-black no-underline block hover:underline"
-            to={"/team/" + 1}
+            className='text-black no-underline block hover:underline'
+            to={'/team/' + 1}
           >
             <p>Team A</p>
           </Link>
         ),
     }),
     columnHelper.accessor((row) => row.company, {
-      id: "Company",
-      header: "Company",
+      id: 'Company',
+      header: 'Company',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor((row) => row.country, {
-      id: "Country",
-      header: "Country",
+      id: 'Country',
+      header: 'Country',
       cell: (info) => info.getValue(),
     }),
-  ];
+    columnHelper.accessor((row) => row.country, {
+      id: 'Total',
+      header: 'Total',
+      cell: (info) => info.getValue(),
+    }),
+  ]
   const table = useReactTable({
     data,
     columns,
@@ -148,27 +154,27 @@ const EmployeeTab = ({ employees }: any) => {
     debugColumns: true,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-  });
+  })
 
   useEffect(() => {
-    console.log(rowSelection);
-    console.log(columnVisibility);
-  });
+    console.log(rowSelection)
+    console.log(columnVisibility)
+  })
 
   const viewEmployeeHandler = (event: any) => {
-    const id: number = event.target.value;
-  };
+    const id: number = event.target.value
+  }
   return (
-    <div className="min-w-fit flex flex-col py-10">
+    <div className='min-w-fit flex flex-col py-10'>
       <ColumnSelect table={table} />
-      <div className="overflow-x-auto shadow-lg rounded-lg">
-        <table className=" text-black">
-          <thead className="bg-blueCegedim text-base whitespace-nowrap text-left">
+      <div className='overflow-x-auto shadow-lg rounded-lg'>
+        <table className=' text-black'>
+          <thead className='bg-blueCegedim text-base whitespace-nowrap text-left'>
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
                   <th key={header.id}>
-                    <div className="mx-2 my-3 text-left">
+                    <div className='mx-2 my-3 text-left'>
                       {header.isPlaceholder
                         ? null
                         : flexRender(
@@ -181,15 +187,15 @@ const EmployeeTab = ({ employees }: any) => {
               </tr>
             ))}
           </thead>
-          <tbody className="text-sm text-center divide-y divide-lightGrey">
+          <tbody className='text-sm text-center divide-y divide-lightGrey'>
             {table.getRowModel().rows.map((row, idx) => (
               <tr
                 key={row.id}
-                className={idx % 2 == 0 ? "bg-lightGrey bg-opacity-30" : ""}
+                className={idx % 2 == 0 ? 'bg-lightGrey bg-opacity-30' : ''}
               >
                 {row.getVisibleCells().map((cell) => (
                   <td key={cell.id}>
-                    <div className="mx-2 py-2 whitespace-nowrap">
+                    <div className='mx-2 py-2 whitespace-nowrap'>
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
@@ -202,59 +208,59 @@ const EmployeeTab = ({ employees }: any) => {
           </tbody>
         </table>
       </div>
-      <div className="flex items-center gap-2 mt-4">
+      <div className='flex items-center gap-2 mt-4'>
         <button
-          className=" rounded px-1 bg-blueCegedim border-none shadow-lg"
+          className=' rounded px-1 bg-blueCegedim border-none shadow-lg'
           onClick={() => table.setPageIndex(0)}
           disabled={!table.getCanPreviousPage()}
         >
-          {"<<"}
+          {'<<'}
         </button>
         <button
-          className="border-none shadow-lg rounded px-2 bg-blueCegedim"
+          className='border-none shadow-lg rounded px-2 bg-blueCegedim'
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
         >
-          {"<"}
+          {'<'}
         </button>
         <button
-          className="border-none shadow-lg rounded px-2 bg-blueCegedim"
+          className='border-none shadow-lg rounded px-2 bg-blueCegedim'
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
-          {">"}
+          {'>'}
         </button>
         <button
-          className=" rounded px-1 bg-blueCegedim border-none shadow-lg"
+          className=' rounded px-1 bg-blueCegedim border-none shadow-lg'
           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
           disabled={!table.getCanNextPage()}
         >
-          {">>"}
+          {'>>'}
         </button>
-        <span className="flex items-center gap-1 whitespace-nowrap">
+        <span className='flex items-center gap-1 whitespace-nowrap'>
           <div>Page</div>
           <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getState().pagination.pageIndex + 1} of{' '}
             {table.getPageCount()}
           </strong>
         </span>
-        <span className="flex items-center gap-1">
+        <span className='flex items-center gap-1'>
           | Go to page:
           <input
-            type="number"
+            type='number'
             defaultValue={table.getState().pagination.pageIndex + 1}
             onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              table.setPageIndex(page);
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
             }}
-            className="border p-1 rounded w-16"
+            className='border p-1 rounded w-16'
           />
         </span>
         <select
-          className="border-2 border-blueCegedim rounded shadow-lg outline-none"
+          className='border-2 border-blueCegedim rounded shadow-lg outline-none'
           value={table.getState().pagination.pageSize}
           onChange={(e) => {
-            table.setPageSize(Number(e.target.value));
+            table.setPageSize(Number(e.target.value))
           }}
         >
           {[10, 20, 30, 40, 50].map((pageSize) => (
@@ -265,6 +271,6 @@ const EmployeeTab = ({ employees }: any) => {
         </select>
       </div>
     </div>
-  );
-};
-export default EmployeeTab;
+  )
+}
+export default EmployeeTab
