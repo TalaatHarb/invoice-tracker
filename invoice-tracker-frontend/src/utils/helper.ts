b6bcad216ee653c5806a28036326680041dfdcaa
something <some@thing.com>
2022-08-31 15:22:54 +0200

    download file with fetch work-around

--- invoice-tracker-frontend/src/utils/helper.ts
+++ invoice-tracker-frontend/src/utils/helper.ts
@@ -9,3 +9,21 @@ export function isValidAttachmentType(type: string) {
       'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
   )
 }
+
+export const downloadFiles = async (downloadLink: string, attachmentName: string, token:string) => {
+    
+
+  const config = {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+  }
+
+  const response = await fetch(downloadLink, config)
+  const data = await response.blob();
+  const a = document.createElement("a");
+  const url = window.URL.createObjectURL(data);
+  a.href = url;
+  a.download = attachmentName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
